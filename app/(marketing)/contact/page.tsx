c9ac8fac162abfb10f4ed4a6854b5f2b81edad14
gauravmeena-prog <gauravmeena-prog@users.noreply.github.com>
2025-09-27 22:46:17 +0530

    🔧 Fix TypeScript error in contact page iframe attributes
    - Changed marginHeight and marginWidth from strings to numbers
    - Fixed height attribute to use number type
    - Resolves Vercel deployment build error

--- app/(marketing)/contact/page.tsx
+++ app/(marketing)/contact/page.tsx
@@ -80,10 +80,10 @@ export default function ContactPage() {
                 <iframe
                   src="https://docs.google.com/forms/d/e/1FAIpQLSemFMa-7r-F4MUG_aacWH-DVAyWUuwHIRTl_oWOXtzRWWlxjw/viewform?embedded=true"
                   width="100%"
-                  height="800"
+                  height={800}
                   frameBorder="0"
-                  marginHeight="0"
-                  marginWidth="0"
+                  marginHeight={0}
+                  marginWidth={0}
                   className="w-full"
                   title="Contact Form"
                   style={{
